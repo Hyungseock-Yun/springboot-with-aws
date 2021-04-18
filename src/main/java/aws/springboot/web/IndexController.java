1df9e23cd14674d13133779558259c49b74a5d70
hsyoon <bagzein@gmail.com>
2021-04-18 14:42:40 +0900

    Apply .gitignore

--- src/main/java/aws/springboot/web/IndexController.java
+++ src/main/java/aws/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package aws.springboot.web;
 
+import aws.springboot.config.auth.LoginUser;
+import aws.springboot.config.auth.dto.SessionUser;
 import aws.springboot.service.posts.PostsService;
 import aws.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,13 @@ public class IndexController {
   private final PostsService postsService;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
+
+    if (user != null) {
+      model.addAttribute("userNames", user.getName());
+    }
+
     return "index";
   }
 
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,5 +1,5 @@
 var main = {
-    init : function () {
+    init: function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
@@ -12,19 +12,19 @@ var main = {
         });
     },
 
-    save : function () {
+    save: function () {
         var data = {
-            title : $('#title').val(),
-            author : $('#author').val(),
-            content : $('#content').val()
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
         };
 
         $.ajax({
-            type : 'POST',
-            url : '/api/v1/posts',
-            dataType : 'json',
-            contentType : 'application/json; charset=utf-8',
-            data : JSON.stringify(data)
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
@@ -33,7 +33,7 @@ var main = {
         });
     },
 
-    update : function () {
+    update: function () {
         var data = {
             title: $('#title').val(),
             content: $('#content').val()
@@ -55,7 +55,7 @@ var main = {
         });
     },
 
-    delete : function () {
+    delete: function () {
         var id = $('#id').val();
 
         $.ajax({
